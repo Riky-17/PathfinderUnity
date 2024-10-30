cb31368de5188d1c3efe8f03db0843d8efae5784
Riccardo <riccardospadaro58@gmail.com>
2024-10-30 17:54:36 +0100

    basic job system implemented

--- AStarPathfinding/Assets/Scripts/PathFinder/Pathfinder.cs
+++ AStarPathfinding/Assets/Scripts/PathFinder/Pathfinder.cs
@@ -10,10 +10,8 @@ public class Pathfinder : MonoBehaviour
 
     //grid fields
     [SerializeField] float gridSizeX = 1;
-    float HalfGridSizeX => gridSizeX / 2;
 
     [SerializeField] float gridSizeZ = 1;
-    float HalfGridSizeZ => gridSizeZ / 2;
 
     int NodesAmountX => (int)math.round(gridSizeX / NodeDiameter);
     int NodesAmountZ => (int)math.round(gridSizeZ / NodeDiameter);
@@ -24,10 +22,6 @@ public class Pathfinder : MonoBehaviour
     float NodeDiameter => nodeRadius * 2;
 
     [SerializeField] LayerMask obstacleLayer;
-
-    NativeList<PathNode> nodesToCheck;
-    NativeList<PathNode> neighbours;
-    NativeHashSet<PathNode> checkedNodes;
 
     void Awake()
     {
@@ -42,7 +36,7 @@ public class Pathfinder : MonoBehaviour
 
     void CreateGrid()
     {
-        gridNodes = new(TotalNodesAmount, Allocator.Temp);
+        gridNodes = new(TotalNodesAmount, Allocator.Persistent);
 
         for (int x = 0; x < NodesAmountX; x++)
         {
@@ -59,54 +53,93 @@ public class Pathfinder : MonoBehaviour
         }
     }
 
-    NativeList<PathNode> GetNeighbourNodes(PathNode node)
-    {
-        NativeList<PathNode> neighbours = new(Allocator.Temp);
-        for (int x = -1; x < 2; x++)
-        {
-            for (int z = -1; z < 2; z++)
-            {
-                int neighbourX = math.clamp(node.x + x, 0, NodesAmountX - 1);                
-                int neighbourZ = math.clamp(node.z + z, 0, NodesAmountZ - 1);
-                int neighbourIndex = neighbourX + neighbourZ * NodesAmountX;
-                
-                if(gridNodes[neighbourIndex] != node)
-                    neighbours.Add(gridNodes[neighbourIndex]);
-            }
-        }
-        return neighbours;
-    }
-
-    bool CheckWorldPosInGrid(Vector3 worldPos, out PathNode node)
-    {
-        if (worldPos.x < -HalfGridSizeX || worldPos.x > HalfGridSizeX || worldPos.z < -HalfGridSizeZ || worldPos.z > HalfGridSizeZ )
-        {
-            // world position is outside of the grid
-            node = default;
-            return false;
-        }
-
-        float tValueX = InverseLerp(-HalfGridSizeX, HalfGridSizeX, worldPos.x);
-        float tValueZ = InverseLerp(-HalfGridSizeZ, HalfGridSizeZ, worldPos.z);
-
-        int x = (int)math.round(tValueX * (NodesAmountX - 1)); 
-        int z = (int)math.round(tValueZ * (NodesAmountZ - 1));
-
-        node = gridNodes[x + z * NodesAmountX];
-        return true; 
-    }
-
     public void FindPath(float3 startingPos, float3 targetPos)
     {
         CreateGrid();
 
-        bool isPathComplete = false;
+        NativeList<float3> path = new(TotalNodesAmount, Allocator.TempJob);
+        NativeList<PathNode> nodesToCheck = new(Allocator.TempJob);
+        NativeHashSet<PathNode> checkedNodes = new(TotalNodesAmount, Allocator.TempJob);
+        NativeList<PathNode> neighbours = new(Allocator.TempJob);
+        PathFinderJob job = new()
+        {
+            gridSizeX = gridSizeX,
+            gridSizeZ = gridSizeZ,
+
+            nodeRadius = nodeRadius,
+
+            startingPos = startingPos,
+            targetPos = targetPos,
+
+            gridNodes = gridNodes,
+            nodesToCheck = nodesToCheck,
+            checkedNodes = checkedNodes,
+            neighbours = neighbours,
+
+            path = path,
+        };
+        
+        JobHandle jobHandle = job.Schedule();
+        jobHandle.Complete();
+        // Debug.Log(job.path[0]);
+        // Debug.Log(job.path[1]);
+        gridNodes.Dispose();
+        nodesToCheck.Dispose();
+        checkedNodes.Dispose();
+        neighbours.Dispose();
+        // Debug.Log(path[0]);
+        PathfinderRequestManager.Instance.FinishedProcessingPath(path, true);
+        path.Dispose();
+    }
+    
+    // void OnDrawGizmos()
+    // {
+    //     Gizmos.DrawWireCube(Vector3.zero, new(gridSizeX, 1, gridSizeZ));
+
+    //     if (gridNodes != null)
+    //     {
+    //         Vector3 cornerOffsets = new(.25f, 0, .25f);
+    //         Vector3 cubeHeight = new(0, .5f, 0);
+
+    //         foreach (PathNode node in gridNodes)
+    //         {
+    //             Gizmos.color = node.IsWalkable ? Color.green : Color.red;
+    //             Gizmos.DrawCube(node.nodePos, Vector3.one - cornerOffsets + cubeHeight);
+    //         }
+    //     }
+    // }
+}
+
+public struct PathFinderJob : IJob
+{
+    public float gridSizeX;
+    readonly float HalfGridSizeX => gridSizeX / 2;
+
+    public float gridSizeZ;
+    readonly float HalfGridSizeZ => gridSizeZ / 2;
+    
+    public float nodeRadius;
+    readonly float NodeDiameter => nodeRadius * 2;
+
+    readonly int NodesAmountX => (int)math.round(gridSizeX / NodeDiameter);
+    readonly int NodesAmountZ => (int)math.round(gridSizeZ / NodeDiameter);
+
+    public float3 startingPos;
+    public float3 targetPos;
+
+    public NativeArray<PathNode> gridNodes;
+
+    public NativeList<PathNode> nodesToCheck;
+    public NativeHashSet<PathNode> checkedNodes;
+    public NativeList<PathNode> neighbours;
+
+    public NativeList<float3> path;
+
+    public void Execute()
+    {
         if(CheckWorldPosInGrid(startingPos, out PathNode startingNode) && startingNode.IsWalkable && CheckWorldPosInGrid(targetPos, out PathNode targetNode) && targetNode.IsWalkable)
         {
-            NativeList<float3> path = new(Allocator.Temp);
-            nodesToCheck = new(Allocator.Temp) { startingNode };
-            neighbours = new(Allocator.Temp);
-            checkedNodes = new(TotalNodesAmount, Allocator.Temp);
+            nodesToCheck.Add(startingNode);
             PathNode currentNode;
 
             while(nodesToCheck.Length > 0)
@@ -134,10 +167,7 @@ public class Pathfinder : MonoBehaviour
 
                 if(currentNode == targetNode)
                 {
-                    isPathComplete = true;
                     targetNode = currentNode;
-                    path = RetracePath(startingNode, targetNode);
-                    path = SimplifyPath(path);
                     break;
                 }
 
@@ -167,62 +197,54 @@ public class Pathfinder : MonoBehaviour
                     }
                 }
             }
-            PathfinderRequestManager.Instance.FinishedProcessingPath(path, isPathComplete);
-            path.Dispose();
-            nodesToCheck.Dispose();
-            neighbours.Dispose();
-            checkedNodes.Dispose();
-        }
-        else
-            PathfinderRequestManager.Instance.FinishedProcessingPath(new(Allocator.Temp), isPathComplete);
-    }
 
-    NativeList<float3> SimplifyPath(NativeList<float3> path)
-    {
-        if (path.Length < 2)
-            return path;
-            
-        NativeList<float3> simplifiedPath = new(Allocator.Temp);
-        float3 prevRelVector = path[1] - path[0];
-
-        for (int i = 1; i < path.Length - 1; i++)
-        {
-            float3 relVector = path[i + 1] - path[i];
-            bool3 isSameDir = math.normalize(relVector) == math.normalize(prevRelVector);
-                
-            if (!isSameDir.x || !isSameDir.y || !isSameDir.z)
-                simplifiedPath.Add(path[i]);
-
-            prevRelVector = relVector;
-        }
-        simplifiedPath.Add(path[^1]);
-        return simplifiedPath;
-    }
-
-    NativeList<float3> RetracePath(PathNode startingNode, PathNode targetNode)
-    {
-        if (checkedNodes.Contains(targetNode))
-        {
-            NativeList<float3> path = new(Allocator.Temp) {targetNode.nodePos};
-            NativeList<float3> retracedPath = new(Allocator.Temp) {};
-
-            PathNode currentNode = targetNode;
-            while (currentNode != startingNode)
+            NativeList<float3> tempPath = RetracePath(startingNode, targetNode);
+            tempPath = SimplifyPath(tempPath);
+            for (int i = 0; i < tempPath.Length; i++)
             {
-                currentNode = gridNodes[currentNode.parentNode];
-                path.Add(currentNode.nodePos);
+                path.Add(tempPath[i]);
             }
-
-            for (int i = path.Length - 1; i >= 0 ; i--)
-                retracedPath.Add(path[i]);
-            
-            return retracedPath;
         }
-        else
-            return new(Allocator.Temp);
     }
 
-    int CalculateDistance(PathNode nodeA, PathNode nodeB)
+    bool CheckWorldPosInGrid(Vector3 worldPos, out PathNode node)
+    {
+        if (worldPos.x < -HalfGridSizeX || worldPos.x > HalfGridSizeX || worldPos.z < -HalfGridSizeZ || worldPos.z > HalfGridSizeZ )
+        {
+            // world position is outside of the grid
+            node = default;
+            return false;
+        }
+
+        float tValueX = InverseLerp(-HalfGridSizeX, HalfGridSizeX, worldPos.x);
+        float tValueZ = InverseLerp(-HalfGridSizeZ, HalfGridSizeZ, worldPos.z);
+
+        int x = (int)math.round(tValueX * (NodesAmountX - 1)); 
+        int z = (int)math.round(tValueZ * (NodesAmountZ - 1));
+
+        node = gridNodes[x + z * NodesAmountX];
+        return true; 
+    }
+    
+    NativeList<PathNode> GetNeighbourNodes(PathNode node)
+    {
+        NativeList<PathNode> neighbours = new(Allocator.Temp);
+        for (int x = -1; x < 2; x++)
+        {
+            for (int z = -1; z < 2; z++)
+            {
+                int neighbourX = math.clamp(node.x + x, 0, NodesAmountX - 1);                
+                int neighbourZ = math.clamp(node.z + z, 0, NodesAmountZ - 1);
+                int neighbourIndex = neighbourX + neighbourZ * NodesAmountX;
+                
+                if(gridNodes[neighbourIndex] != node)
+                    neighbours.Add(gridNodes[neighbourIndex]);
+            }
+        }
+        return neighbours;
+    }
+
+    readonly int CalculateDistance(PathNode nodeA, PathNode nodeB)
     {
         int distanceX = math.abs(nodeA.x - nodeB.x);
         int distanceZ = math.abs(nodeA.z - nodeB.z);
@@ -232,8 +254,8 @@ public class Pathfinder : MonoBehaviour
         
         return 14 * distanceX + 10 * (distanceZ - distanceX);
     }
-
-    bool IsNodePastCorner(PathNode currentNode, PathNode currentNeighbour)
+    
+    readonly bool IsNodePastCorner(PathNode currentNode, PathNode currentNeighbour)
     {
         foreach (PathNode neighbour in neighbours)
         {
@@ -247,23 +269,52 @@ public class Pathfinder : MonoBehaviour
         }
         return false;
     }
-
-    float InverseLerp(float a, float b, float c) => (c - a) / (b - a);
     
-    // void OnDrawGizmos()
-    // {
-    //     Gizmos.DrawWireCube(Vector3.zero, new(gridSizeX, 1, gridSizeZ));
+    readonly NativeList<float3> SimplifyPath(NativeList<float3> path)
+    {
+        if (path.Length < 2)
+            return path;
+        NativeList<float3> simplifiedPath = new(Allocator.Temp);
+        float3 prevRelVector = path[1] - path[0];
 
-    //     if (gridNodes != null)
-    //     {
-    //         Vector3 cornerOffsets = new(.25f, 0, .25f);
-    //         Vector3 cubeHeight = new(0, .5f, 0);
+        for (int i = 1; i < path.Length - 1; i++)
+        {
+            float3 relVector = path[i + 1] - path[i];
+            bool3 isSameDir = math.normalize(relVector) == math.normalize(prevRelVector);
+                
+            if (!isSameDir.x || !isSameDir.y || !isSameDir.z)
+            {
+                simplifiedPath.Add(path[i]);
+            }
 
-    //         foreach (PathNode node in gridNodes)
-    //         {
-    //             Gizmos.color = node.IsWalkable ? Color.green : Color.red;
-    //             Gizmos.DrawCube(node.nodePos, Vector3.one - cornerOffsets + cubeHeight);
-    //         }
-    //     }
-    // }
+            prevRelVector = relVector;
+        }
+        simplifiedPath.Add(path[^1]);
+        return simplifiedPath;
+    }
+    
+    NativeList<float3> RetracePath(PathNode startingNode, PathNode targetNode)
+    {
+        if (checkedNodes.Contains(targetNode))
+        {
+            NativeList<float3> path = new(Allocator.Temp) {targetNode.nodePos};
+
+            PathNode currentNode = targetNode;
+            while (currentNode != startingNode)
+            {
+                currentNode = gridNodes[currentNode.parentNode];
+                path.Add(currentNode.nodePos);
+            }
+
+            NativeList<float3> retracedPath = new(Allocator.Temp);
+            for (int i = path.Length - 1; i >= 0 ; i--)
+                retracedPath.Add(path[i]);
+            
+            return retracedPath;
+        }
+        else
+            return new(Allocator.Temp);
+    }
+    
+    readonly float InverseLerp(float a, float b, float c) => (c - a) / (b - a);
 }
